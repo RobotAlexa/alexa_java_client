37c582c70573addcc4912cc37c50f2b714b2ac58
Lin Hui <bright.lin@ubtechedu.com>
2018-06-20 18:32:47 +0800

    resolve 'ctrl + c' signal

--- YansheeControl/main.py
+++ YansheeControl/main.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from net import network_manager
+import signal
+
+
+def bye_bye(signum, frame):
+    print("\nYanshee control server exit.\n")
+    exit()
+
 
 def main():
     network_manager.start()
 
+
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, bye_bye)
+    signal.signal(signal.SIGTERM, bye_bye)
+    signal.signal(signal.SIGABRT, bye_bye)
+    signal.signal(signal.SIGILL, bye_bye)
     main()

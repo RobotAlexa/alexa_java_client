3d2ed7792db8a63c5332590ffd1310afe28a1c5d
Lin Hui <bright.lin@ubtechedu.com>
2018-06-20 18:14:30 +0800

    fix bug: some cmd not work

--- YansheeControl/net/network_manager.py
+++ YansheeControl/net/network_manager.py
@@ -19,37 +19,37 @@ def is_json(json_str):
 
 
 def start():
+    print ("Yanshee control server starting.")
     # connect SDK
     controller.connect(ip)
 
     server.listen(5)
-    need_exit = False
     while True:
-        print ("Yanshee control server start.")
+        print ("Yanshee control server listening.")
         conn, addr = server.accept()
         try:
-            while 1:
-                client_data = conn.recv(4096)
-                str_data = str(client_data)
-                print ("receive message: %s" % str_data)
-                if is_json(str_data):
-                    request = json.loads(str_data)
+            client_data = conn.recv(4096)
+            str_data = str(client_data)
+            print ("receive message: %s" % str_data)
+            if is_json(str_data):
+                request = json.loads(str_data)
 
-                    if cmd_cmd in request and request[cmd_cmd].strip() == cmd_exit:
-                        need_exit = True
-                    else:
-                        cmd_parser(request)
+                if cmd_cmd in request and request[cmd_cmd].strip() == cmd_exit:
+                    break
+                else:
+                    cmd_parser(request)
+            conn.close()
 
         except Exception, e:
             print ("Error: %s" % e)
             conn.close()
 
-        if need_exit:
-            break
     print ("Socket close.")
 
     # disconnect SDK
     controller.disconnect(ip)
+
+    print ("Yanshee control server stop.")
 
 
 def cmd_parser(request):
@@ -74,36 +74,36 @@ def cmd_parser(request):
         }
     '''
     if cmd_cmd in request:
-        if cmd_action in request:
+        if request[cmd_cmd] == cmd_action and obj_action in request:
             # action
-            action = request[cmd_action]
+            action = request[obj_action]
             repeat = 1
             if key_name in action:
                 if key_repeat in action:
                     repeat = action[key_repeat]
                 controller.do_action(action[key_name], repeat)
 
-        elif cmd_dance in request:
+        elif request[cmd_cmd] == cmd_dance and obj_dance in request:
             # dance
-            dance = request[cmd_dance]
+            dance = request[obj_dance]
             repeat = 1
             if key_name in dance:
                 if key_repeat in dance:
                     repeat = dance[key_repeat]
                 controller.do_dance(dance[key_name], repeat)
 
-        elif cmd_moving in request:
+        elif request[cmd_cmd] == cmd_moving and obj_moving in request:
             # moving
-            moving = request[cmd_moving]
+            moving = request[obj_moving]
             repeat = 1
             if key_name in moving:
                 if key_repeat in moving:
                     repeat = moving[key_repeat]
                 controller.do_moving(moving[key_name], repeat)
 
-        elif cmd_volume in request:
+        elif request[cmd_cmd] == cmd_volume and obj_volume in request:
             # set volume
-            volume = request[cmd_volume]
+            volume = request[obj_volume]
             if key_direction in volume:
                 direction = volume[key_direction]
                 if direction == direction_up:
@@ -111,5 +111,5 @@ def cmd_parser(request):
                 else:
                     controller.do_volume_down()
 
-        elif cmd_stop in request:
+        elif request[cmd_cmd] == cmd_stop:
             controller.do_stop()
